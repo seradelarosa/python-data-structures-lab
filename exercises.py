4521825d14b8877143057462b4d641d6e30f4872
Sera De La Rosa <sera.delarosa@outlook.com>
2025-03-28 13:34:59 -0400

    first exercise done.

---
+++ exercises.py
@@ -0,0 +1,41 @@
+# Exercise 0: Example
+#
+# This is a practice exercise to familiarize you with basic Python data structures.
+#
+# Create a list called `example_list` and append three elements to it. Print each element using a loop.
+#
+# Requirements:
+# - The list should contain any three elements of your choice.
+# - Use a loop to print each element.
+
+def example_list_function():
+  example_list = ['element1', 'element2', 'element3']
+  for element in example_list:
+      print(element)
+
+# Call the function and print each element
+example_list_function()
+
+##################
+##################
+
+# Exercise 1: List and Indexing
+#
+# Create a list named students containing at least three student names (strings).
+# Assign the second student’s name to a variable named first_student.
+# Assign the last student’s name to a variable named last_student.
+
+def manage_students():
+    # your code here
+    students = ['Sera', 'Matt', 'Grey', 'Bel']
+    first_student = students[1]
+    # grab last value instead of hardcoding position
+    last_student = students[-1]
+    # need to return values!
+    return first_student, last_student
+
+# Call the function and print the result
+print('Exercise 1:', manage_students())
+
+
+
